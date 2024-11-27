79ad60bdcc4c95a14e4944f7b5df0b49d09bde1b
dvnfirefox <134988327+dvnfirefox@users.noreply.github.com>
2024-11-26 20:11:06 -0500

    first commiy

---
+++ src/test/java/org/example/projectoop/ProjectOopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
